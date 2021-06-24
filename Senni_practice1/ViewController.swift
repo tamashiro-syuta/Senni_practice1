e1585ba9aefe3432c1df9eb6b8935fb616a2ae50
tamashiro_syuta <syuta5825@gmail.com>
2021-06-24 21:01:29 +0900

    Initial Commit

---
+++ Senni_practice1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Senni_practice1
+//
+//  Created by 玉城秀大 on 2021/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
